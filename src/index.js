447461ce47ac1cc75cc75c6b1300d89a2d99f41b
antonshwab <shwab.ay@gmail.com>
2018-05-04 15:00:58 +0300

    Init golos chain

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import golos from 'golos-js';
+
+golos.config.set('websocket', 'wss://ws.testnet.golos.io');
+golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
